c0648b6d5bc613d6e7c8ea1c60d4f8aa9c1cea18
samuelriveiraescudero <samuelriveiraescudero@gmail.com>
2024-06-16 20:17:12 +0000

    Todos los insertar funcionando y mejorables

--- interfaz_ventana.py
+++ interfaz_ventana.py
@@ -24,51 +24,6 @@ class ObtenerDetalles:
     def mostrar_detalles(self):
         return self.title, self.description
 
-class InsertarScreen(Screen):
-    CSS_PATH = "insertar.tcss"
-
-    def compose(self) -> ComposeResult:
-        self.titulo_input = Input(placeholder="Título", id="titulo_input", value="")
-        self.contenido_input = Input(placeholder="Contenido", id="contenido_input", value="")
-        self.confirmacion_button = Button("Crear", variant="error", id="confirmacion")
-        self.cancelar_button = Button("Cancelar", variant="primary", id="cancelar")
-
-        yield self.titulo_input
-        yield self.contenido_input
-        yield self.confirmacion_button
-        yield self.cancelar_button
-
-    async def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "cancelar":
-            self.app.pop_screen()
-        elif event.button.id == "confirmacion":
-            self.app.push_screen(QuitScreen(self.titulo_input.value, self.contenido_input.value))
-                
-
-class QuitScreen(Screen):
-    def __init__(self, titulo_input_value, contenido_input_value):
-        super().__init__()
-        self.titulo_input_value = titulo_input_value
-        self.contenido_input_value = contenido_input_value
-
-    def compose(self) -> ComposeResult:
-        return [
-            Grid(
-                Label("¿Estás seguro de que quieres actualizar la nota?", id="question"),
-                Button("Confirmar", variant="error", id="confirmar"),
-                Button("Cancelar", variant="primary", id="cancel"),
-                id="dialog",
-            )
-        ]
-
-    async def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "confirmar":
-            cp = ColeccionPendientes()
-            cp.insertar(self.titulo_input_value, self.contenido_input_value)
-            self.app.pop_screen()
-        elif event.button.id == "cancel":
-            self.app.pop_screen()
-
 
 class PendientesScreen(Screen):
 
@@ -118,19 +73,22 @@ class En_ProcesoScreen(Screen):
     ]
 
     def action_insertar(self):
-        pass
+
+        self.app.push_screen(InsertarScreen2())
+        
 
     def compose(self) -> ComposeResult:
         global current_title
         global current_description
+
         with Container(id="main_container"):
             yield Label(current_title, id="titulo")
-            with Container(id="contenedor-EnProceso"):
-                yield Static(current_description, id="EnProceso")
+            with Container(id="contenedor-descripcion"):
+                yield Static(current_description, id="descripcion")
         yield Footer()
 
     def action_volver(self):
-        pass
+        self.app.pop_screen()
 
     def action_actualizar(self):
         pass
@@ -150,22 +108,157 @@ class TerminadosScreen(Screen):
     ]
 
     def action_insertar(self):
-        pass
+
+        self.app.push_screen(InsertarScreen3())
+        
 
     def compose(self) -> ComposeResult:
         global current_title
         global current_description
+
         with Container(id="main_container"):
             yield Label(current_title, id="titulo")
-            with Container(id="contenedor-Terminado"):
-                yield Static(current_description, id="Terminado")
+            with Container(id="contenedor-descripcion"):
+                yield Static(current_description, id="descripcion")
         yield Footer()
 
     def action_volver(self):
-        pass
+        self.app.pop_screen()
 
     def action_actualizar(self):
         pass
 
     def action_borrar(self):
         pass
+
+class InsertarScreen(Screen):
+    CSS_PATH = "insertar.tcss"
+
+    def compose(self) -> ComposeResult:
+        self.titulo_input = Input(placeholder="Título", id="titulo_input", value="")
+        self.contenido_input = Input(placeholder="Contenido", id="contenido_input", value="")
+        self.confirmacion_button = Button("Crear", variant="error", id="confirmacion")
+        self.cancelar_button = Button("Cancelar", variant="primary", id="cancelar")
+
+        yield self.titulo_input
+        yield self.contenido_input
+        yield self.confirmacion_button
+        yield self.cancelar_button
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "cancelar":
+            self.app.pop_screen()
+        elif event.button.id == "confirmacion":
+            self.app.push_screen(QuitScreen(self.titulo_input.value, self.contenido_input.value))
+                
+class QuitScreen(Screen):
+    def __init__(self, titulo_input_value, contenido_input_value):
+        super().__init__()
+        self.titulo_input_value = titulo_input_value
+        self.contenido_input_value = contenido_input_value
+
+    def compose(self) -> ComposeResult:
+        return [
+            Grid(
+                Label("¿Estás seguro de que quieres actualizar la nota?", id="question"),
+                Button("Confirmar", variant="error", id="confirmar"),
+                Button("Cancelar", variant="primary", id="cancel"),
+                id="dialog",
+            )
+        ]
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "confirmar":
+            cp = ColeccionPendientes()
+            cp.insertar(self.titulo_input_value, self.contenido_input_value)
+            self.app.pop_screen()
+        elif event.button.id == "cancel":
+            self.app.pop_screen()
+
+class InsertarScreen2(Screen):
+    CSS_PATH = "insertar.tcss"
+
+    def compose(self) -> ComposeResult:
+        self.titulo_input = Input(placeholder="Título", id="titulo_input", value="")
+        self.contenido_input = Input(placeholder="Contenido", id="contenido_input", value="")
+        self.confirmacion_button = Button("Crear", variant="error", id="confirmacion")
+        self.cancelar_button = Button("Cancelar", variant="primary", id="cancelar")
+
+        yield self.titulo_input
+        yield self.contenido_input
+        yield self.confirmacion_button
+        yield self.cancelar_button
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "cancelar":
+            self.app.pop_screen()
+        elif event.button.id == "confirmacion":
+            self.app.push_screen(QuitScreen2(self.titulo_input.value, self.contenido_input.value))
+                
+class QuitScreen2(Screen):
+    def __init__(self, titulo_input_value, contenido_input_value):
+        super().__init__()
+        self.titulo_input_value = titulo_input_value
+        self.contenido_input_value = contenido_input_value
+
+    def compose(self) -> ComposeResult:
+        return [
+            Grid(
+                Label("¿Estás seguro de que quieres actualizar la nota?", id="question"),
+                Button("Confirmar", variant="error", id="confirmar"),
+                Button("Cancelar", variant="primary", id="cancel"),
+                id="dialog",
+            )
+        ]
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "confirmar":
+            cep = ColeccionEnProceso()
+            cep.insertar(self.titulo_input_value, self.contenido_input_value)
+            self.app.pop_screen()
+        elif event.button.id == "cancel":
+            self.app.pop_screen()
+
+class InsertarScreen3(Screen):
+    CSS_PATH = "insertar.tcss"
+
+    def compose(self) -> ComposeResult:
+        self.titulo_input = Input(placeholder="Título", id="titulo_input", value="")
+        self.contenido_input = Input(placeholder="Contenido", id="contenido_input", value="")
+        self.confirmacion_button = Button("Crear", variant="error", id="confirmacion")
+        self.cancelar_button = Button("Cancelar", variant="primary", id="cancelar")
+
+        yield self.titulo_input
+        yield self.contenido_input
+        yield self.confirmacion_button
+        yield self.cancelar_button
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "cancelar":
+            self.app.pop_screen()
+        elif event.button.id == "confirmacion":
+            self.app.push_screen(QuitScreen3(self.titulo_input.value, self.contenido_input.value))
+                
+class QuitScreen3(Screen):
+    def __init__(self, titulo_input_value, contenido_input_value):
+        super().__init__()
+        self.titulo_input_value = titulo_input_value
+        self.contenido_input_value = contenido_input_value
+
+    def compose(self) -> ComposeResult:
+        return [
+            Grid(
+                Label("¿Estás seguro de que quieres actualizar la nota?", id="question"),
+                Button("Confirmar", variant="error", id="confirmar"),
+                Button("Cancelar", variant="primary", id="cancel"),
+                id="dialog",
+            )
+        ]
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "confirmar":
+            ct = ColeccionTerminados()
+            ct.insertar(self.titulo_input_value, self.contenido_input_value)
+            self.app.pop_screen()
+        elif event.button.id == "cancel":
+            self.app.pop_screen()
